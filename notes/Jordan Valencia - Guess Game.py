7c0e5843361cf034b8ae4e17e1071e663b72d86e
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2018-11-02 08:41:49 -0700

    Im done with the guessing game

--- notes/Jordan Valencia - Guess Game.py
+++ notes/Jordan Valencia - Guess Game.py
@@ -9,3 +9,43 @@ elif guess_number < random1:
     print("You lose, %s was actually your number." % random1)
 elif guess_number == random1:
     print("You win!")
+random1 = (random.randint(0, 10))
+
+guess_number = int(input("Guess a number between 0 to 10 "))
+
+if guess_number > random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number < random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number == random1:
+    print("You win!")
+random1 = (random.randint(0, 10))
+
+guess_number = int(input("Guess a number between 0 to 10 "))
+
+if guess_number > random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number < random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number == random1:
+    print("You win!")
+random1 = (random.randint(0, 10))
+
+guess_number = int(input("Guess a number between 0 to 10 "))
+
+if guess_number > random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number < random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number == random1:
+    print("You win!")
+random1 = (random.randint(0, 10))
+
+guess_number = int(input("Guess a number between 0 to 10 "))
+
+if guess_number > random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number < random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number == random1:
+    print("You win!")
