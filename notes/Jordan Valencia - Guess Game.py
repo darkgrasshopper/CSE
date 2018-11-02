5fad5821a2507e0348aca1b3be53e352a961c67e
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2018-11-02 08:33:30 -0700

    I finished the guess game.

--- notes/Jordan Valencia - Guess Game.py
+++ notes/Jordan Valencia - Guess Game.py
@@ -1,2 +1,11 @@
 import random
-print(random.randint(0, 10))
+random1 = (random.randint(0, 10))
+
+guess_number = int(input("Guess a number between 0 to 10 "))
+
+if guess_number > random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number < random1:
+    print("You lose, %s was actually your number." % random1)
+elif guess_number == random1:
+    print("You win!")
