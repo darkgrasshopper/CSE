7e377a588c934cf9935c1fb1e3c9290e0c352868
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2018-11-07 08:17:03 -0800

    I finished the guess game

--- notes/Jordan Valencia - Guess Game.py
+++ notes/Jordan Valencia - Guess Game.py
@@ -1,51 +1,17 @@
 import random
 random1 = (random.randint(0, 10))
+win = False
+guess_left = 5
 
-guess_number = int(input("Guess a number between 0 to 10 "))
 
-if guess_number > random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number < random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number == random1:
-    print("You win!")
-random1 = (random.randint(0, 10))
-
-guess_number = int(input("Guess a number between 0 to 10 "))
-
-if guess_number > random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number < random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number == random1:
-    print("You win!")
-random1 = (random.randint(0, 10))
-
-guess_number = int(input("Guess a number between 0 to 10 "))
-
-if guess_number > random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number < random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number == random1:
-    print("You win!")
-random1 = (random.randint(0, 10))
-
-guess_number = int(input("Guess a number between 0 to 10 "))
-
-if guess_number > random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number < random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number == random1:
-    print("You win!")
-random1 = (random.randint(0, 10))
-
-guess_number = int(input("Guess a number between 0 to 10 "))
-
-if guess_number > random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number < random1:
-    print("You lose, %s was actually your number." % random1)
-elif guess_number == random1:
-    print("You win!")
+while guess_left > 0 and not win:
+    guess_number = int(input("Guess a number between 0 to 10 "))
+    if guess_number > random1:
+        print("Guess lower")
+        guess_left -= 1
+    elif guess_number < random1:
+        print("Guess higher")
+        guess_left -= 1
+    elif guess_number == random1:
+        print("You win!")
+        win = True
