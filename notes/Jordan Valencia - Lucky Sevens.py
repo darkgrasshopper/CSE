fae0b8216ebca2cc7fbdb4df574fcfff387d9ba5
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2018-11-14 08:43:24 -0800

    Today I did lucky sevens

--- notes/Jordan Valencia - Lucky Sevens.py
+++ notes/Jordan Valencia - Lucky Sevens.py
@@ -5,4 +5,11 @@ money = 15
 while money >0:
     dice_1 = random.randint(1, 6)
     dice_2 = random.randint(1, 6)
-    
+    print(dice_1, dice_2)
+
+    if dice_1 + dice_2 == 7:
+        money += 4
+        print("Congrat, you now have", money, "dollars now.")
+    elif dice_1 + dice_2 < 7:
+        money -= 1
+        print("You lost. You now have", money, "dollars")
