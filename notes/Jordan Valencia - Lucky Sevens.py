b02492ef7c11efb7fde669d4223a73e3774efda6
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2018-11-15 08:24:27 -0800

    I finished Lucky Sevens

--- notes/Jordan Valencia - Lucky Sevens.py
+++ notes/Jordan Valencia - Lucky Sevens.py
@@ -1,15 +1,26 @@
 import random
 
+rounds_max_money = 0
 money = 15
-
+round = 0
+max_money = 15
 while money >0:
     dice_1 = random.randint(1, 6)
     dice_2 = random.randint(1, 6)
-    print(dice_1, dice_2)
+  # print(dice_1, dice_2)
+
+    round += 1
+
+    money -= 1
 
     if dice_1 + dice_2 == 7:
-        money += 4
+        money += 5
         print("Congrat, you now have", money, "dollars now.")
+        if money > max_money:
+            max_money = money
+            rounds_max_money = round
     elif dice_1 + dice_2 < 7:
-        money -= 1
         print("You lost. You now have", money, "dollars")
+print("you lasted",round,"rounds")
+print("you had",max_money,"dollars")
+print("You had",rounds_max_money,"rounds with max money")
