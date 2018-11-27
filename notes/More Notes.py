2c4ec306e969c12ced44dc39babad366eb2691b7
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2018-11-27 08:45:28 -0800

    Did more notes

--- notes/More Notes.py
+++ notes/More Notes.py
@@ -31,6 +31,56 @@ print(new_list[1:4])
 print(new_list[1:])
 print(new_list[:2])
 
+#Adding things to a list
+holiday_list = []  #ALWAYS USE SQUARE BRACKETS FOR LISTS
+holiday_list.append("Tacos")
+holiday_list.append("Bumblebee")
+holiday_list.append("Red Dead Redemption 2")
+print(holiday_list)
+
+# Notice this is object.method(Parameter)"
+
+# Removing things from a list
+holiday_list.remove("Tacos")
+print(holiday_list)
 
 
+holiday_list = []
+holiday_list.append("Math Book")
+holiday_list.append("Women")
+holiday_list.append("Dogs")
+holiday_list.append("Computer")
+print(holiday_list)
+
+holiday_list.remove("Math Book")
+print(holiday_list)
+
+# ALSO removing things from a list
+holiday_list.pop(0) #Removes the item at index 0
+print(holiday_list)
+
+#Tuble
+brands = ("apple","Samsung","HTC") # notice the parenthesis
+
+colors = ['blue', 'green','red','purple','cyan','white'
+    ,'gold','violet','black','teal','orange']
+print(len(colors))
+
+# Find the index
+print(colors.index("gold"))
+
+# Changing things into a list
+string1 = "green"
+list1 = list(string1)
+print(list1)
+
+for character in list1:
+    if character == "e":
+        # replace with a *
+        current_index = list1.index(character)
+        list1.pop(current_index)
+        list1.insert(current_index, "*")
+
+# Changing lists into strings
+print("".join(list1))
 
