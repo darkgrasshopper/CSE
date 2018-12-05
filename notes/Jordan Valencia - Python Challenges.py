6fd217d2d556633b34dc7c700ca3eca12a046820
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2018-12-05 08:43:40 -0800

    I did some challenges

--- notes/Jordan Valencia - Python Challenges.py
+++ notes/Jordan Valencia - Python Challenges.py
@@ -1,5 +1,155 @@
-def challenge7(number):
+def challenge1(first_name, last_name):
+    print(last_name, first_name)
+
+challenge1("Brian","Jack")
+
+
+def challenge2(numbers):
+   print(numbers)
+   if numbers % 2 == 0:
+       print("The number is even.")
+   else:
+       print("The number is odd.")
+
+
+challenge2(50)
+
+
+def challenge3(base, height):
+    print(base*height/2)
+
+challenge3(203, 92)
+
+
+def challenge4(number):
+
+
+
+ def challenge7(number):
     print(number+number*number+number**3)
 
 
-challenge7(2)
+ challenge7(2)
+
+def pythagorean(a, b):
+    return (a**2 + b**2)**(1/2)
+
+
+print(pythagorean(3, 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
