64f266b70de6634283ece1c03842ea41d547e731
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2019-01-08 08:44:23 -0800

    NOtes

---
+++ notes/Semester 2 Notes-Jordan Valencia.py
@@ -0,0 +1,19 @@
+print("Hello World")
+#dawggofollowlol
+
+cars = 5
+driving = True
+print("I have %s cars" % cars)
+
+print("I have %d cars" % cars)
+print("I have" + str(cars) + "cars")
+age = input("how old are you?")
+print(age + "? My daughter is older than you.")
+
+list1 = ["red","blue","green","purple","black"]
+print(list1)
+list1.append("pink")
+print(list1)
+
+list1.pop(0)
+print(list1)
