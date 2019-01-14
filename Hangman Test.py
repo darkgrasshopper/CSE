14dfe9ee13639c57c18493b6a28cd4c306a77189
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2019-01-14 08:43:35 -0800

    i did sum work

---
+++ Hangman Test.py
@@ -0,0 +1,10 @@
+import random
+letters_guessed = []
+guess_left = 0
+guess_total = 8
+word_bank = ["Air"]
+word_selected = random.choice(word_bank)
+random.choice(word_bank)
+
+
+
--- Hangman-Jordan Valencia.py
+++ Hangman-Jordan Valencia.py
@@ -1,5 +1,9 @@
 import random
+guesses_taken = 0
 wordbank = ["Please","jannet","give","me","back","the","kids","please","please","please"]
 random.choice(wordbank)
+print("Pick a letter")
+
+
 
 
