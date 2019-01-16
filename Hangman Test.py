1aa46ca39944d021a5b3a369329ae35bacc0081e
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2019-01-16 08:42:30 -0800

    I tried making some things
    I tried to make it able to pick a letter from my word bank

--- Hangman Test.py
+++ Hangman Test.py
@@ -1,15 +1,12 @@
 import random
 import string
 print(list(string.ascii_letters))
-print(string.digits)
-print(string.punctuation)
 letters_guessed = []
 guess_left = 0
 guess_total = 8
 word_bank = ["Air"]
 word_selected = random.choice(word_bank)
 random.choice(word_bank)
-print("")
-for i in range(len("Air")):
-    print = (len("Air"))
+for i in range(len(word_bank)):
+    word_selected = random.choice(word_bank)
 
--- Hangman-Jordan Valencia.py
+++ Hangman-Jordan Valencia.py
@@ -1,8 +1,15 @@
 import random
+import string
+print(list(string.ascii_letters))
 guesses_taken = 0
-wordbank = ["Please","jannet","give","me","back","the","kids","please","please","please"]
-random.choice(wordbank)
+guess_total = 8
+word_bank = ["Please","jannet","give","me","back","the","kids","please","please","please"]
+random.choice(word_bank)
 print("Pick a letter")
+input(word_bank)
+word_bank.append(input('Please'))
+
+
 
 
 
