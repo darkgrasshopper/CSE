368b4db36dd9d458346212ab6114b1bd2ecada93
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2019-01-31 08:44:03 -0800

    Took Notes

--- notes/Dictionary World Map - Jordan Valencia.py
+++ notes/Dictionary World Map - Jordan Valencia.py
@@ -17,3 +17,28 @@ world_map = {
         }
     }
 }
+
+
+#OtherVariables
+
+current_node = world_map['R19A']
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+playing = True
+
+
+
+
+#Controller
+while playing:
+    print(current_node["NAME"])
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.upper() in directions:
+        try:
+         room_name = current_node['PATHS'][command.upper()]
+        current_node = world_map[room_name]
+    except KeyError:
+        print("I can't go that way")
+else:
+   print("Command not recognized")
