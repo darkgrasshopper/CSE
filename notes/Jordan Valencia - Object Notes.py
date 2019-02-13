38fb99fd93441bdd3d9a4fad523ae6d7bc9c9a9f
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2019-02-13 08:42:33 -0800

    We took more notess

--- notes/Jordan Valencia - Object Notes.py
+++ notes/Jordan Valencia - Object Notes.py
@@ -1,5 +1,7 @@
+import Special_Random
+
 class WaterGun(object):
-      def __init__(self, capacity, distance, stock):
+    def __init__(self, capacity, distance=30, stock=False):
         # These are things that a WaterGun has.
         # All of us these should be relevant to our program.
         self.capacity = capacity
@@ -8,18 +10,34 @@ class WaterGun(object):
         self.duration_of_pressure = 5
         self.stock = stock
 
-
     def shoot(self, time):
         if self.trigger:
             if self.duration_of_pressure <= 0:
                 print("There's no pressure")
-        elif self.duration_of_pressure < time:
-            print("You run out of pressure after firing for %s seconds", self.duration_of_pressure)
-            self.duration_of_pressure = 0
-    else:
- print("You shoot for %s seconds" % time)
-        self.duration_of_pressure -= time
- print("There's no trigger!")
+            elif self.duration_of_pressure < time:
+                print("You run out of pressure after firing for %s seconds", self.duration_of_pressure)
+                self.duration_of_pressure = 0
+            else:
+                print("You shoot for %s seconds" % time)
+                self.duration_of_pressure -= time
+        else:
+            print("There's no trigger!")
+    def pump_it_up(self):
+        self.duration_of_pressure = 5
+    print("You pump the tank back to full pressure")
 
-my_water_gun = WaterGun()
 
+my_water_gun = WaterGun(5.2, 40, True)
+your_water_gun = WaterGun(1.0, 1, False)
+wiebe_water_gun = WaterGun(999999999999, 9999999999999999999, True)
+yahir_water_gun = WaterGun(0.1)
+
+
+# Do stuff with the objects
+my_water_gun.shoot(5)
+wiebe_water_gun.shoot(5)
+your_water_gun.shoot(5)
+yahir_water_gun.shoot(6)
+my_water_gun.pump_it_up()
+
+print(Special_Random.RandomWiebe.special_random())
