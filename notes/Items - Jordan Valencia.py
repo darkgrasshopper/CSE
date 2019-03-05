582899515d34a61c5d9e201954970fba40782198
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2019-03-05 08:42:07 -0800

    i got some progress on my 15 objects

--- notes/Items - Jordan Valencia.py
+++ notes/Items - Jordan Valencia.py
@@ -1,2 +1,13 @@
 class Item(object):
-    def __init__(self):
+            def __init__(self, name, material):
+                self.name = name
+                self.material = material
+
+
+class Sword(Item):
+            def __init__(self, name, damage, decription, duration, length):
+                self.name
+                self.damge
+                self.description
+                self.duration
+                self.length
