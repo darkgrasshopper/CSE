b9d218e49096d69db6e9067b3421c84918865eba
darkgrasshopper <42383971+darkgrasshopper@users.noreply.github.com>
2019-03-21 08:43:06 -0700

    I finished putting my items in

--- notes/World Map OOP- Jordan Valencia.py
+++ notes/World Map OOP- Jordan Valencia.py
@@ -30,33 +30,151 @@ class Item(object):
 
 
 class Weapon(Item):
-    def __init__(self, name, damage, material, protection, effectivity):
+    def __init__(self, name, damage, material,):
         super(Weapon, self).__init__(name, material)
         self.damage = damage
-        self.protection = protection
-        self.effectivity = effectivity
 
 
 class Consumables(Item):
-    def __init__(self, name, heal, material, description):
+    def __init__(self, name, heal, poison, description, material):
         super(Consumables, self).__init__(name, material)
         self.heal = heal
         self.description = description
+        self.poison = poison
+
+
+class HealthPotionLvl1(Consumables):
+    def __init__(self, name, heal, poison, description):
+        super(HealthPotionLvl1, self).__init__(name, heal, poison, description, None)
+        self.heal = 10
+        self.description = "it heals you 10 health"
+        self.poison = None
+
+
+class HealthPotionLvl2(Consumables):
+    def __init__(self, name, heal, poison, description):
+        super(HealthPotionLvl2, self).__init__(name, heal, poison, description, None)
+        self.heal = 15
+        self.description = "it heals you 15 health"
+        self.poison = None
+
+
+class HealthPotionLvl3(Consumables):
+    def __init__(self, name, heal, poison, description):
+        super(HealthPotionLvl3, self).__init__(name, heal, poison, description, None)
+        self.heal = 20
+        self.poison = None
+        self.description = "it heals you 20 health"
+
+
+class PoisonPotionLvl1(Consumables):
+    def __init__(self, name, heal, poison, description):
+        super(PoisonPotionLvl1, self).__init__(name, heal, poison, description, None)
+        self.heal = None
+        self.poison = 10
+        self.description = "it takes away 10 health"
+
+
+class PoisonPotionLvl2(Consumables):
+    def __init__(self, name, heal, poison, description):
+        super(PoisonPotionLvl2, self).__init__(name, heal, poison, description, None)
+        self.heal = None
+        self.poison = 15
+        self.description = "it takes away 15 health"
+
+
+class PoisonPotionLvl3(Consumables):
+    def __init__(self, name, heal, poison, description):
+        super(PoisonPotionLvl3, self).__init__(name, heal, poison, description, None)
+        self.heal = None
+        self.poison = 20
+        self.description = "it takes away 20 health"
 
 
 class Sword(Weapon):
     def __init__(self, name, material, damage, description, duration, length):
-        super(Sword, self).__init__(name, damage, material, damage, description)
+        super(Sword, self).__init__(name, damage, material)
         self.description = description
         self.duration = duration
         self.length = length
 
 
+class StoneSword(Sword):
+    def __init__(self, name, material, damage, description, duration, length):
+        super(StoneSword, self).__init__(name, material, damage, description, duration, length)
+        self.damage = 10
+        self.description = "It inflicts 10 damage"
+        self.duration = 1
+        self.length = "medium"
+
+
+class GoldSword(Sword):
+    def __init__(self, name, material, damage, description, duration, length):
+        super(GoldSword, self). __init__(name, material, damage, description, duration, length)
+        self.damage = 20
+        self.description = "It inflicts 20 damage"
+        self.duration = 2
+        self.length = "medium"
+
+
+class IronSword(Sword):
+    def __init__(self, name, material, damage, description, duration, length):
+        super(IronSword, self).__init__(name, material, damage, description, duration, length)
+        self.damage = 30
+        self.description = "It inflicts 30 damage"
+        self.duration = 2.5
+        self.length = "long"
+
+
+class SatansSword(Sword):
+    def __init__(self, name, material, damage, description, duration, length):
+        super(SatansSword, self).__init__(name, material, damage, description, duration, length)
+        self.damage = 30
+        self.description = "It inflicts 55 damage"
+        self.duration = 4.0
+        self.length = "long"
+
+
 class Shield(Weapon):
     def __init__(self, name, material, protection, description):
-        super(Shield, self).__init__(name, None, material, protection, description)
+        super(Shield, self).__init__(name, None, material)
         self.protection = protection
         self.description = description
+
+
+class WoodenShield(Shield):
+    def __init__(self, name, material, protection, description):
+        super(WoodenShield, self).__init__(name, material, protection, description)
+        self.protection = 5
+        self.description = "this shield offers 5 protection"
+
+
+class SteelShield(Shield):
+    def __init__(self, name, material, protection, description):
+        super(SteelShield, self).__init__(name, material, protection, description)
+        self.protection = 10
+        self.description = "this shield offers 10 protection"
+
+
+class GlassShield(Shield):
+    def __init__(self, name, material, protection, description):
+        super(GlassShield, self).__init__(name, material, protection, description)
+        self.protection = 11
+        self.description = "this shield offers 11 protection"
+
+
+class GoldShield(Shield):
+    def __init__(self, name, material, protection, description):
+        super(GoldShield, self).__init__(name, material, protection, description)
+        self.protection = 15
+        self.description = "this shield offers 15 protection"
+
+
+class DiamondShield(Shield):
+    def __init__(self, name, material, protection, description):
+        super(DiamondShield, self). __init__(name, material, protection, description)
+        self.protection = 25
+        self.description = "this shield offers 25 protection"
 
 
 class Tool(Item):
@@ -66,23 +184,18 @@ class Tool(Item):
         self.description = description
 
 
-health_potion_lvl1 = Consumables("Health Potion lvl 1", 10, None, "it heals you for 10 health")
-health_potion_lvl2 = Consumables("Health Potion lvl 2", 15, None, "it heals you for 15 health")
-health_potion_lvl3 = Consumables("Health Potion lvl 3", 20, None, "it heals you for 20 health")
-poison_potion_lvl1 = Consumables("Poison Potion lvl 1", 10, None, "it takes away 10 health")
-poison_potion_lvl2 = Consumables("Poison Potion lvl 2", 15, None, "it takes away 15 health")
-poison_potion_lvl3 = Consumables("Poison Potion lvl 3", 20, None, "it takes away 20 health")
-stone_sword = Sword("Stone Sword", "Stone", 10, "It inflicts 10 damage", 1, "medium")
-gold_sword = Sword("Gold Sword", "Gold", 20, "It inflicts 20 damage", 2, "medium")
-iron_sword = Sword("Iron Sword", "Iron", 30, "It inflicts 30 damage", 2.5, "long")
-satans_sword = Sword("Satan's Sword", "Souls", 30, "It inflicts 30 damage", 4, "long")
-wooden_shield = Shield("Wooden Shield", "Wood", 5, "this shield offers 5 protection")
-steel_shield = Shield("Steel Shield", "Steel", 10, "this shield offers 10 protection")
-glass_shield = Shield("Glass Shield", "Glass", 11, "this shield offers 11 protection")
-gold_shield = Shield("Gold Shield", "Gold", 15, "this shield offers 15 protection")
-diamond_shield = Shield("Diamond Shield", "Diamonds", 25, "this shield offers 25 protection")
-stone_shovel = Tool("Stone Shovel", "Stone", 5, "this stone shovel helps you dig with a 5 efficiency")
-stone_pickAxe = Tool("Stone PickAxe", "Stone", 5, "this stone pickAxe helps you mine with a 5 efficiency")
+class StoneShovel(Tool):
+    def __init__(self, name, material, efficacy, description):
+        super(StoneShovel, self). __init__(name, material, efficacy, description)
+        self.efficacy = 5
+        self.description = "this stone shovel helps you dig with a 5 effectivity"
+
+
+class StonePickAxe(Tool):
+    def __init__(self, name, material, efficacy, description):
+        super(StonePickAxe, self). __init__(name, material, efficacy, description)
+        self.efficacy = 5
+        self.description = "this Stone PickAxe helps you mine with a 5 effectivity"
 
 
 class Character(object):
@@ -128,16 +241,16 @@ playing = True
 directions = ['north', 'south', 'east', 'west', 'up', 'down']
 
 # Items
-sword = Weapon("Sword", 15, None, None, None)
-sword2 = Weapon("Orc Sword", 5, None, None, None)
+StoneSword = StoneSword("Stone Sword", "Stone", 10, "This sword has inflicted 10 damage", 1, "medium")
+IronSword = IronSword("Iron Sword", "Stone", 30, "This sword has inflicted 30 damage", 2.5, "long")
 
 
 # Characters
-c1 = Character("Orc1", 100, sword, None)
-c2 = Character("Orc2", 100, sword2, None)
+c1 = Character("Orc1", 100, StoneSword, None)
+c2 = Character("Orc2", 100, IronSword, None)
 c1.attack(c2)
 c2.attack(c1)
-
+player = Character("You", 100, SatansSword, None)
 
 # Controller
 while playing:
